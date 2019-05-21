acecd7944a0217d26d5888c350d25b0dfbdef5a4
Rhioda muthie <rhiodam@gmail.com>
2019-05-21 14:23:38 +0700

    Managing the Application Lifecycle with Spring Boot

--- src/main/java/com/rhiodamuthie/sbapirestful/SbApiRestfulApplication.java
+++ src/main/java/com/rhiodamuthie/sbapirestful/SbApiRestfulApplication.java
@@ -3,23 +3,41 @@ package com.rhiodamuthie.sbapirestful;
 import com.rhiodamuthie.sbapirestful.model.Quote;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 public class SbApiRestfulApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(SbApiRestfulApplication.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(SbApiRestfulApplication.class, args);
+    }
 
     private static final Logger log = LoggerFactory.getLogger(SbApiRestfulApplication.class);
 
-    public static void main(String args[]) {
-        RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info(quote.toString());
+//    public static void main(String args[]) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//        log.info(quote.toString());
+//    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 
 }
