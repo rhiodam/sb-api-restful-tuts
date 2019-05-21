c30276ad3a848967a344923f2e0739439669037c
Rhioda muthie <rhiodam@gmail.com>
2019-05-21 14:11:43 +0700

    Initial spring boot project

---
+++ src/main/java/com/rhiodamuthie/sbapirestful/SbApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.sbapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApiRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbApiRestfulApplication.class, args);
+    }
+
+}
