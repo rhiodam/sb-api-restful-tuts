49eac276e3c5ff5a4f37c9adca2c60421f22de09
Rhioda muthie <rhiodam@gmail.com>
2019-05-21 14:20:51 +0700

    Make the application executable

--- src/main/java/com/rhiodamuthie/sbapirestful/SbApiRestfulApplication.java
+++ src/main/java/com/rhiodamuthie/sbapirestful/SbApiRestfulApplication.java
@@ -1,13 +1,25 @@
 package com.rhiodamuthie.sbapirestful;
 
-import org.springframework.boot.SpringApplication;
+import com.rhiodamuthie.sbapirestful.model.Quote;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class SbApiRestfulApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SbApiRestfulApplication.class, args);
+//    public static void main(String[] args) {
+//        SpringApplication.run(SbApiRestfulApplication.class, args);
+//    }
+
+    private static final Logger log = LoggerFactory.getLogger(SbApiRestfulApplication.class);
+
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
     }
 
 }
